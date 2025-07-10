02659ce5733b9f69c4d905981662a73e32d375bf
wireside <wiresifed@gmail.com>
2025-07-11 02:09:06 +0300

    new: created weather package and implemented displaying weather info

--- main.go
+++ main.go
@@ -3,20 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
-	
+
 	"demo/weather/geo"
+	"demo/weather/weather"
 )
 
 func main() {
 	city := flag.String("city", "", "Город пользователя")
-	// format := flag.Int("format", 1, "Формат вывода погоды")
+	format := flag.Int("format", 1, "Формат вывода погоды")
 	flag.Parse()
-	
+
 	geoData, err := geo.GetMyLocation(*city)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	
-	fmt.Println(geoData)
+
+	weatherData := weather.GetWeather(*geoData, *format)
+	fmt.Println(weatherData)
 }
--- weather/weather.go
+++ weather/weather.go
@@ -1,1 +1,40 @@
 package weather
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	
+	"demo/weather/geo"
+)
+
+func GetWeather(geoData geo.GeoData, format int) string {
+	baseUrl, err := url.Parse("https://wttr.in/" + geoData.City)
+	if err != nil {
+		fmt.Println(err.Error())
+		return ""
+	}
+	
+	params := url.Values{}
+	params.Add("format", fmt.Sprint(format))
+	baseUrl.RawQuery = params.Encode()
+	
+	res, err := http.Get(baseUrl.String())
+	if err != nil {
+		fmt.Println(err.Error())
+		return ""
+	}
+	if res.StatusCode != 200 {
+		fmt.Println(errors.New("request failed: status code is not 200"))
+		return ""
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		return ""
+	}
+	
+	return string(body)
+}

b18dd5af2353f83e417d05ffedff95806e4fe56f
wireside <wiresifed@gmail.com>
2025-07-11 00:19:42 +0300

    new: created geo package for getting user location

---
+++ geo/geo.go
@@ -0,0 +1,41 @@
+package geo
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type GeoData struct {
+	City string `json:"city"`
+}
+
+func GetMyLocation(city string) (*GeoData, error) {
+	if city != "" {
+		return &GeoData{
+			City: city,
+		}, nil
+	}
+	
+	res, err := http.Get("https://ipapi.co/json/")
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != 200 {
+		return nil, errors.New("request failed: status code is not 200")
+	}
+	
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	
+	var geoData GeoData
+	err = json.Unmarshal(body, &geoData)
+	if err != nil {
+		return nil, err
+	}
+	
+	return &geoData, nil
+}
--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	
+	"demo/weather/geo"
 )
 
 func main() {
 	city := flag.String("city", "", "Город пользователя")
-	format := flag.Int("format", 1, "Формат вывода погоды")
-	
+	// format := flag.Int("format", 1, "Формат вывода погоды")
 	flag.Parse()
 	
-	fmt.Println(*city)
-	fmt.Println(*format)
+	geoData, err := geo.GetMyLocation(*city)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	
+	fmt.Println(geoData)
 }
